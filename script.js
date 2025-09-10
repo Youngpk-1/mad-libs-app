42cd12e5adb348dfa9bab5e14f3dcf612abdc392
isaacwatson30 <isaacwatson30@gmail.com>
2025-09-09 21:59:43 -0500

    fixed errors and corrected script and helpers order

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-onEvent("next", "click", function () {
-  var name = getText("name");
-  var age = getText("Age");
-  var position = getText("Position");
+onEvent("submit", "click", function () {
+  var name = getValue("name");
+  var age = getValue("Age");
+  var position = getValue("Position");
+  var userMessage =
+    "Hi  " +
+    name +
+    ", Wow, " +
+    age +
+    " looks good on you! When holding a " +
+    position +
+    " job title, its important to remember that it is more than just a label, its a reflection of your responsibilities, professionalism, and career trajectory.\nMaintaining a strong professional image requires clarity, communication, and a proactive approach to managing both your duties and your work-life balance.";
   console.log(name);
   console.log(age);
   console.log(position);
-setText(
-    "Textarea1",
-    "Hi  " +
-      name +
-      ", Wow, " +
-      age +
-      " looks good on you! When holding a " +
-      position +
-      " job title, its important to remember that it is more than just a label, its a reflection of your responsibilities, professionalism, and career trajectory.\nMaintaining a strong professional image requires clarity, communication, and a proactive approach to managing both your duties and your work-life balance."
-  );
+  setText("text1", userMessage);
 });
